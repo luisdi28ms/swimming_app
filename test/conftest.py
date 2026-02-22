c83890f2954c0732bd93c0dbf9abb291d397765b
Luis Monge <luisdi28ms@gmail.com>
2026-02-21 18:24:30 -0600

    fix test

---
+++ test/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import os
+from src.main import app, db
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['UPLOAD_FOLDER'] = '/tmp/test_uploads'
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+            yield client
+            db.drop_all()
